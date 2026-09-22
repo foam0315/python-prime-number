b833d83d8ec594eac6b363390a6b89c52fbfd1f9
foam0315 <a01045666684@gmail.com>
2026-09-22 17:24:01 +0900

    feat: math.sqrt를 이용한 소수 판별 연산 최적화

--- python-prime-number/prime-number.py
+++ python-prime-number/prime-number.py
@@ -1,11 +1,15 @@
-# prime-number.py - 소수 구하기 기본 로직 완성
-print("1부터 100 사이의 소수를 구합니다.")
+import math
+
+print("1부터 100 사이의 소수 (최적화 버전):")
+
 for num in range(2, 101):
     is_prime = True
-    for i in range(2, num):
-        if num % i == 0:
+
+    for divisor in range(2, int(math.sqrt(num)) + 1):
+        if num % divisor == 0:
             is_prime = False
             break
+
     if is_prime:
         print(num, end=" ")
         
